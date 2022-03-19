85c92ce920797fbe4c41861ccac868fddfc168e4
duyanh3110 <duyanh19962012@gmail.com>
2022-03-19 23:24:25 +0700

    remove gitignore

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  googleClientID:
+    "594479314226-u90ksk7kqi09nqesce1f0g9sfjvt3qm0.apps.googleusercontent.com",
+  googleClientSecret: "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",
+};
