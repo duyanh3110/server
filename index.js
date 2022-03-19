b86987735bcdf0f8caafeae3c764af4102ea7d67
duyanh3110 <duyanh19962012@gmail.com>
2022-03-19 23:15:19 +0700

    passport.js

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
+const passport = require("passport");
+const { googleClientID, googleClientSecret } = require("./config/keys");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "buddy" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: googleClientID,
+      clientSecret: googleClientSecret,
+      callbackURL: "/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("access token: ", accessToken);
+      console.log("refresh token: ", refreshToken);
+      console.log("profile: ", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get("/auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
